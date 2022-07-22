9a7176519bf004912ec121c7c4115b863683add5
Mariohdpz <mariohdpz@gmail.com>
2022-07-21 19:56:05 -0500

    Map drawer

--- src/components/zonePicker/index.js
+++ src/components/zonePicker/index.js
@@ -3,28 +3,38 @@ import { LinkContainer } from "react-router-bootstrap";
 
 import { MapContainer } from 'react-leaflet/MapContainer'
 import { TileLayer } from 'react-leaflet/TileLayer'
+import { FeatureGroup } from 'react-leaflet/FeatureGroup'
+
+import { EditControl, } from "react-leaflet-draw"
 
 import "./index.scss";
-import { Marker } from "react-leaflet";
-
-import { Popup } from "react-leaflet";
-
+import "leaflet-draw/dist/leaflet.draw.css"
 
 function ZonePicker() {
     return (
         <Container className="p-3 text-center">
             <h1 className="header">Elige tu zona:</h1>
             <Container className="p-3 text-center zone-picker">
-                <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+                <MapContainer center={[22.878, -106.260]} zoom={6} scrollWheelZoom={false}>
                     <TileLayer
                         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                     />
-                    <Marker position={[51.505, -0.09]}>
-                        <Popup>
-                            A pretty CSS3 popup. <br /> Easily customizable.
-                        </Popup>
-                    </Marker>
+                    <FeatureGroup>
+                        <EditControl
+                            position='topright'
+                            onEdited={() => {}}
+                            onCreated={() => {}}
+                            onDeleted={() => {}}
+                            draw={{
+                                rectangle: false,
+                                polyline: false,
+                                circle: false,
+                                marker: false,
+                                circlemarker: false
+                            }}
+                        />
+                    </FeatureGroup>
                 </MapContainer>
             </Container>
             <LinkContainer to="/zone-picker">
